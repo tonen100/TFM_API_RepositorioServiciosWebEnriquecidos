ea27bf69a38bb7bfdd5685250a6163c5d3566057
Pierre-François Giraud <pierref.giraud@insa-rouen.fr>
2020-04-10 16:24:30 +0200

    Inserted first user at setup time for CI

--- setupDevMongo.js
+++ setupDevMongo.js
@@ -1,2 +1,8 @@
 use API_Repository
 db.createUser({ user: "admin", pwd: "mdp", roles: [{ role: "readAnyDatabase", db: "admin" }] })
+db.users.save({
+    "username": "Test",
+    "email": "admin@test.com",
+    "password": "mdp",
+    "role": "Administrator"
+});
